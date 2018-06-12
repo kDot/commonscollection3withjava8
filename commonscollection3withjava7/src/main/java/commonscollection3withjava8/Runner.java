99dbd4a70e005796e2bc97a60705ab01e8c772e8
klaus <k-Laus@gmx.net>
2018-06-12 07:05:32 +0200

    Java 7 version

---
+++ commonscollection3withjava7/src/main/java/commonscollection3withjava8/Runner.java
@@ -0,0 +1,30 @@
+package commonscollection3withjava8;
+
+import java.util.logging.Logger;
+
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
+
+public class Runner {
+
+	static Logger logger = Logger.getAnonymousLogger();
+
+	public static void main(String[] args) {
+		logger.info("Used Java version: "+System.getProperty("java.runtime.version") + "\r\n");
+
+		MultiValueMap openbanktransfers = new MultiValueMap();
+		openbanktransfers.put("key1", "value1");
+		openbanktransfers.put("key2", "value2");
+		logger.info("Beginning: " + openbanktransfers.toString() + "\r\n");
+
+		openbanktransfers.remove("key1", "value1");
+
+		logger.info("Result after removal with MultiValueMap reference: " + openbanktransfers.toString() + "\r\n");
+
+		MultiMap someMultiMap = openbanktransfers;
+		someMultiMap.remove("key2", "value2");
+
+		logger.info("Result after removal with MultiMap reference: " + openbanktransfers.toString() + "\r\n");
+
+	}
+}
